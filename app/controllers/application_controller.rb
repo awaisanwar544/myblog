7b44aad84fb08c89d1c54928303caf1254cd6e1c
awaisanwar544 <awaisanwar544@gmail.com>
2022-06-11 15:20:17 +0500

    Process data in models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   def hello
     render html: 'Hello World'
   end
+
+  def current_user
+    User.first
+  end
+
+  def self.current_user
+    User.first
+  end
 end

d7610599b43be21fca5d570d08c6068742bc97fc
awaisanwar544 <awaisanwar544@gmail.com>
2022-06-11 16:55:29 +0500

    Add views for Users and Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,14 @@
 class PostsController < ApplicationController
   def index
+    user_index = params['user_id'].to_i
+    @user_info = User.find(user_index)
+    @posts = Post.includes(:comments, comments: [:user]).where(user_id: user_index).order(id: :desc)
   end
 
   def show
+    user_index = params['user_id'].to_i
+    post_index = params['id'].to_i
+    @user_info = User.find(user_index)
+    @post_info = Post.find(post_index)
   end
 end

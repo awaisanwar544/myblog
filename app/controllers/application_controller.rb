4cd5e0c5cb4a74ff8faf9020402cbf0183b1570b
awaisanwar544 <awaisanwar544@gmail.com>
2022-06-14 22:57:28 +0500

    Update user registration form Update Registration parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: 'Hello World'
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name posts_counter])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
             numericality: { only_integer: true, greater_than_or_equal_to: 0,
                             message: 'postsCounter must be integer and >=0' }
 
+  after_initialize :set_defaults
+
   def three_most_recent_posts
     # method returns the 3 most recent posts by this user
     Post.where('user_id = ?', id).order(created_at: :desc).limit(3)
@@ -20,5 +22,11 @@ class User < ApplicationRecord
   def self.three_most_recent_posts(user)
     # method returns the 3 most recent posts for the given user
     Post.where('user_id = ?', user.id).order(created_at: :desc).limit(3)
+  end
+
+  private
+
+  def set_defaults
+    self.postsCounter ||= 0
   end
 end
